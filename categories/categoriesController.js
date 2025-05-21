48a21f9a69ecc86ca16ab1b0b1268ed26d054802
Richard PC <rifernun@gmail.com>
2025-05-21 10:54:28 -0300

    add do footer, header e navbar, alem de comeco do admin do categories

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const router = express.Router();
 
-router.get("/categories", (req, res) => { //usar em arquivos que nao sao o principal
-    res.send("Rota de categorias.")
-})
-router.get("/admin/categories/new", (req, res) => { //usar em arquivos que nao sao o principal
-    res.send("Rota para criar uma nova categoria.")
-})
+router.get("/admin/categories/new", (req,res) => {
+    res.render("admin/categories/new");
+});
 
 module.exports = router;

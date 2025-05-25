7e4fc11472e2011a8bd21f726a3ebc03407fef99
Richard PC <rifernun@gmail.com>
2025-05-25 10:06:41 -0300

    Adicao do edit e algumas modificacoes

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/");
+            res.redirect("/admin/categories");
         });
     }else{
         res.redirect("/admin/categories/new");
@@ -45,6 +45,41 @@ router.post("/categories/delete", (req,res) => {
     }else{
         res.redirect("/admin/categories");
     }
+});
+
+router.get("/admin/categories/edit/:id", (req,res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render("admin/categories/edit", {
+                category: category
+            });
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    });
+});
+
+router.post("/categories/update", (req,res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    category.update({
+        title: title,
+        slug: slugify(title)
+    },{where: {
+        id: id
+    }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    })
 });
 
 module.exports = router;

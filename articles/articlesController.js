538ffa8841f0a1caccf1e208fa35ada776868b45
Richard PC <rifernun@gmail.com>
2025-05-26 09:55:38 -0300

    Adicionando o tinymce para textarea, alem de adicao da home page, edit, delete e index do articles

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,11 +1,89 @@
 const express = require('express')
 const router = express.Router();
+const Category = require('../categories/category');
+const Article = require('./article');
+const slugify = require('slugify');
 
-router.get("/articles", (req, res) => { //usar em arquivos que nao sao o principal
-    res.send("Rota de artigos.")
-})
-router.get("/admin/articles/new", (req, res) => { //usar em arquivos que nao sao o principal
-    res.send("Rota para criar um novo artigo.")
-})
+router.get("/admin/articles", (req, res) => { //usar em arquivos que nao sao o principal
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then(articles => {
+        res.render("admin/articles/index", { articles: articles });
+    })  
+});
+
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    });
+});
+
+router.post("/articles/save", (req,res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+});
+
+router.post("/articles/delete", (req,res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){ //for numero
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(()=>{
+                res.redirect("/admin/articles");
+            });
+            
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }else{
+        res.redirect("/admin/articles");
+    }
+});
+
+router.get("/admin/articles/edit/:id", (req,res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {article:article, categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
+router.post("/articles/update", (req,res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({title: title, body: body, categoryId: category, slug: slugify(title)}, { 
+        where: {
+            id: id
+        }
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    }).catch( err => {
+        console.log(err)
+        res.redirect("/");
+    })
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -29,7 +29,56 @@ app.use("/", articlesController);
 
 
 app.get("/", (req,res) => {
-    res.render("index");
+    Article.findAll({
+        order: [
+            ['id','DESC']
+        ]
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        });  
+    })
+})
+
+app.get("/:slug", (req,res) => {
+    var slug = req.params.slug;
+    Article.findOne({ 
+        where: {
+            slug: slug
+    }
+    }).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            });  
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    })
+})
+
+app.get("/category/:slug", (req,res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => { //se achar a categoria, faz isso
+        if(category != undefined){
+
+            Category.findAll().then(categories => {
+                res.render("index", {articles: category.articles, categories: categories}); //acessando o foreign key com include (join)
+            })
+
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    })
 })
 
 app.listen(8080, () => {

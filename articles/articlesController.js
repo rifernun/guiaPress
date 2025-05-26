597bc5085510cb0c8523806f6af1dd8abf6f6969
Richard PC <rifernun@gmail.com>
2025-05-26 20:15:39 -0300

    logica da paginacao

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -86,4 +86,38 @@ router.post("/articles/update", (req,res) => {
     })
 });
 
+router.get("/articles/page/:num", (req,res) => {
+    var page = req.params.num;
+    var offset = 0;
+
+    if(isNaN(page) || page == 1){
+        offset = 0
+    }else{
+        offset = parseInt(page) * 4;
+    }
+
+     //retorna todos os artigos e a quantidade que tem
+    Article.findAndCountAll({
+        limit: 4, 
+        offset: offset 
+    }).then(articles => {
+
+        var next;
+        if(offset * 4 >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+        var result = {
+            next: next,
+            articles: articles
+        }
+
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page", { result: result, categories: categories});
+        });
+        
+    })
+});
+
 module.exports = router;
